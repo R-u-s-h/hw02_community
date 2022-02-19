ddf8ba678d4994b41a9f505afb4579c5627646ca
Aleksey Levadniy <levadniy@inbox.ru>
2022-02-20 01:34:53 +0300

    sprint 3 end - fix bugs 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,6 +4,7 @@ from .models import Group, Post
 
 POSTS_LIMIT_ON_PAGE = 10
 
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:POSTS_LIMIT_ON_PAGE]
     template = 'posts/index.html'
