559a57a54af42f56d7b47c1eb2fa6d0ef9fe019b
Aleksey Levadniy <levadniy@inbox.ru>
2022-02-20 01:21:19 +0300

    sprint 3 end - fix bugs 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post, Group
-
+from .models import Group, Post
 
 POSTS_LIMIT_ON_PAGE = 10
 
