32e85be49de8b5a80fb27e823cebba6c77df8a4b
Aleksey Levadniy <levadniy@inbox.ru>
2022-02-20 13:53:23 +0300

    sprint 3 end - fix bugs 3

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -2,15 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 SECRET_KEY = '@1z$mpk6gtpk&cf$ei+wk9l^t(yth92fk&u=8&)jb#=rh3o@14'
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 INSTALLED_APPS = [
     'posts.apps.PostsConfig',
@@ -54,16 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -80,8 +72,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -91,8 +81,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
